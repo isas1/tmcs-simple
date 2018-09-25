f4eb23a950995646decd73921da1af25515142d2
isas1 <teachmecomputerscience@gmail.com>
2018-09-25 17:24:34 +0100

    Component refactor

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
 import React, { Component } from "react";
-import {
-  Route,
-  NavLink,
-  BrowserRouter
-} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Container from "./components/Container/Container";
 import Typewriter from "./Typewriter";
 import Logo from "./components/Logo/logo.jsx";
 
-const styles = {
-  float: "right",
-  display: "inline",
-  listStyleType: "none",
-  margin: 0,
-}
+import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       
       <div>
-        <ul style={styles}>
+        <ul id="logo">
           <li><Logo /> </li>
         </ul>   
         <BrowserRouter>
           <div>
           <h1><Typewriter /></h1>
-            <ul className="header">
-              <li><NavLink exact to="/">Home</NavLink></li>
-              <li><NavLink to="/about">About</NavLink></li>
-              <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
-            <div className="content">
-              <Route exact path="/" component={Home}/>
-              <Route path="/about" component={About}/>
-              <Route path="/contact" component={Contact}/>
-            </div>
+            <Header />
+            <Container />
           </div>
         </BrowserRouter>
       </div>
