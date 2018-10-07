e297f9baa7b49a0f59ce690b701130fd0ab012c7
isas1 <teachmecomputerscience@gmail.com>
2018-10-07 20:33:24 +0100

    browserHistory removed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import Header from "./components/Header/Header";
 import Container from "./components/Container/Container";
 import Typewriter from "./Typewriter";
 import Logo from "./components/Logo/logo.jsx";
-import createHistory from "history/createBrowserHistory";
-
 import './App.css';
-
-
 
 class App extends Component {
   render() {
-    // console.log(process.env.PUBLIC_URL);
     return (
-      
       <div>
         <ul id="logo">
           <li><Logo /> </li>
-        </ul>   
-        <BrowserRouter history={createHistory({ basename: process.env.PUBLIC_URL })}>
+        </ul>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <div>
           <h1><Typewriter /></h1>
             <Header />
